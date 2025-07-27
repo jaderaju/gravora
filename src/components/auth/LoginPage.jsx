f8a88d6cd698a87401caf8ba47f709952fe1aef0
sathya Narayana <47709520+jaderaju@users.noreply.github.com>
2025-07-27 12:50:14 +0300

    Update LoginPage.jsx

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
-import { supabase } from 'src/lib/supabase.js'; // ensure this path is correct
+import { supabase } from '../../lib/supabase.js';
 
 const LoginPage = ({ onLogin }) => {
   const [email, setEmail] = useState('');
